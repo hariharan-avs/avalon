50a0c7752e0f3d7aaa9b08a75ae37e096d4d01c8
subramanian <hariharanx.subramanian@intel.com>
2019-11-06 08:58:00 +0530

    1106 0857

--- tests/validation_suite/worker_tests/test_demo.py
+++ tests/validation_suite/worker_tests/test_demo.py
@@ -32,7 +32,7 @@ def test_worker_update(setup_config):
 
     # process worker update
     request_tup = (input_json_file, input_type, tamper, output_json_file_name,
-                   uri_client, request_method, worker_obj, sig_obj,
+                   uri_client, request_method, worker_obj, 
                    request_id, check_update_result)
 
     response_tup = validate_request(request_tup)
@@ -40,7 +40,7 @@ def test_worker_update(setup_config):
     err_cd = response_tup[0]
     input_update = response_tup[1]
     response = response_tup[2]
-    
+
     if err_cd == 0:
         logger.info('''Test Case Success : Worker Update request
                     Processed successfully \n''')
