75b75ada6601fdfe8e5d93d7038361577833680d
subramanian <hariharanx.subramanian@intel.com>
2019-11-06 13:57:51 +0530

    1106 0157

--- tests/validation_suite/worker_tests/test_demo.py
+++ tests/validation_suite/worker_tests/test_demo.py
@@ -48,3 +48,43 @@ def test_worker_update(setup_config):
                     failed to process successfully \n''')
 
     assert err_cd == 0
+
+def test_worker_update_invalid_parameter(setup_config):
+    """ Testing worker update request with all valid parameter values. """
+
+    # retrieve values from conftest session fixture
+    worker_obj = setup_config[0]
+    uri_client = setup_config[1]
+    private_key = setup_config[2]
+    err_cd = setup_config[3]
+
+    # input and output names
+    input_json_file = './worker_tests/input/worker_update_invalid_parameter.json'
+    input_type = 'file'
+    output_json_file_name = 'worker_update_invalid_parameter'
+    tamper = {"params": {}}
+    request_method = ""
+    request_id = 0
+
+    # expected response
+    check_update_result = {"error": {"code": 2}}
+
+    # process worker update
+    request_tup = (input_json_file, input_type, tamper, output_json_file_name,
+                   uri_client, request_method, worker_obj,
+                   request_id, check_update_result)
+
+    response_tup = validate_request(request_tup)
+
+    err_cd = response_tup[0]
+    input_update = response_tup[1]
+    response = response_tup[2]
+
+    if err_cd == 0:
+        logger.info('''Test Case Success : Worker Update request
+                    Processed successfully \n''')
+    else:
+        logger.info('''Test Case Failure : Worker Update request
+                    failed to process successfully \n''')
+
+    assert err_cd == 0
