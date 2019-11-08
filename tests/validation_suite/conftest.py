278aaf7b1fb76bbef91f51fc21aab3545117081c
subramanian <hariharanx.subramanian@intel.com>
2019-11-08 09:33:53 +0530

    1108 0933

--- tests/validation_suite/conftest.py
+++ tests/validation_suite/conftest.py
@@ -105,7 +105,7 @@ def worker_lookup_retrieve(config, worker_obj, uri_client):
     output_json_file_name = 'worker_lookup'
 
     lookup_obj = WorkerLookUp()
-    lookup_obj.set_worker_type(WorkerType.TEE_SGX)
+    lookup_obj.set_worker_type(worker_type=WorkerType.TEE_SGX)
     input_worker_look_up = json.loads(lookup_obj.to_string())
 
     # input_json_str = input_worker_look_up
